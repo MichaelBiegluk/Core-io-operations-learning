7e3edbc4613746a5e4edd53a81fc1cdeec687cf0
Michał Paweł Biegluk <134352883+MichaelBiegluk@users.noreply.github.com>
2024-10-09 00:57:05 +0200

    Create AOSP.js
    Signed-off-by: Michał Paweł Biegluk <134352883+MichaelBiegluk@users.noreply.github.com>

---
+++ AOSP.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright macwater90@gmail.com The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.foo;
+
+import android.os.Blah;
+import android.view.Yada;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Does X and Y and provides an abstraction for Z.
+ */
+
+public class Foo {
+    ...
+}
